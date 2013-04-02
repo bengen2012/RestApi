83cb63969a4dbd947f77ba46d0a011f2c5b022c1
bengen2012 <bengen@126.com>
2013-04-02 18:33:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 require_once("client/request.php");
 $url = 'test.ben.dev.jinpu.com/server/interface.php?';
-$url = $url.$_SERVER['QUERY_STRING'];
 $firstRequst = new Request($url,'GET',array('a'=>'aaaaaaaaaaaaa'));
 $firstRequst->execute();
 print_r($firstRequst->getResponseBody());
